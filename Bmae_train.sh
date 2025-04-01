b69ad7cff82b71b16f72e74a15d839befdd58a52
Harold-Finchwxx <3367341605@qq.com>
2025-04-01 16:36:05 +0800

    更新Bmae_train.sh脚本，修改了执行权限，优化了MAE模型训练流程，增加了第二个MAE的训练配置，并调整了相关参数设置。

--- Bmae_train.sh
+++ Bmae_train.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash --login 
 
 # 设置CUDA设备
 export CUDA_VISIBLE_DEVICES=0
@@ -6,21 +6,35 @@ export CUDA_VISIBLE_DEVICES=0
 # 创建输出目录
 mkdir -p output_dir/bootstrapped_mae
 
-# 训练Bootstrapped MAE模型
+# 训练第一个MAE
 python main_bootstrapped_pretrain.py \
-    --model bootstrapped_mae_tiny_patch4_dec96d4b \
     --batch_size 256 \
-    --epochs 200 \
-    --accum_iter 1 \
-    --input_size 32 \
+    --model bootstrapped_mae_tiny_patch4_dec96d4b \
+    --num_mae 2 \
+    --current_mae_idx 0 \
+    --epochs_per_mae 100 \
     --mask_ratio 0.75 \
-    --data_path ./data/cifar10 \
-    --output_dir ./output_dir/bootstrapped_mae \
-    --log_dir ./output_dir/bootstrapped_mae \
+    --accum_iter 1 \
+    --warmup_epochs 20 \
     --blr 1e-3 \
     --weight_decay 0.05 \
-    --warmup_epochs 40 \
-    --num_workers 4 \
-    --target_model ./output_dir/mae/checkpoint-199.pth \
-    --use_ema \
-    --ema_decay 0.999 
+    --output_dir ./output_dir/bootstrapped_mae/mae_0 \
+    --log_dir ./output_dir/bootstrapped_mae/mae_0 \
+    --data_path ./data/cifar10
+
+# 训练第二个MAE
+python main_bootstrapped_pretrain.py \
+    --batch_size 256 \
+    --model bootstrapped_mae_tiny_patch4_dec96d4b \
+    --num_mae 2 \
+    --current_mae_idx 1 \
+    --epochs_per_mae 100 \
+    --mask_ratio 0.75 \
+    --accum_iter 1 \
+    --warmup_epochs 20 \
+    --blr 1e-3 \
+    --weight_decay 0.05 \
+    --output_dir ./output_dir/bootstrapped_mae/mae_1 \
+    --log_dir ./output_dir/bootstrapped_mae/mae_1 \
+    --data_path ./data/cifar10 \
+    --prev_mae_path ./output_dir/bootstrapped_mae/mae_0/checkpoint-99.pth 
