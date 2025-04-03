c3c6da9252faa9a8a547ec2555157ebb167cdcba
Harold-Finchwxx <3367341605@qq.com>
2025-04-03 16:21:04 +0800

    调整ema_mae_train.sh脚本中的学习率参数，从1e-4修改为1e-3，以优化训练效果。

--- ema_mae_train.sh
+++ ema_mae_train.sh
@@ -14,7 +14,7 @@ python main_ema_pretrain.py \
     --model ema_mae_vit_tiny_patch4 \
     --epochs 100 \
     --accum_iter 1 \
-    --blr 1e-4 \
+    --blr 1e-3 \
     --weight_decay 0.05 \
     --output_dir ./output_dir/ema_mae/adamw/ed0.999_lr1e-3_wd0.05_mask0.75 \
     --log_dir ./output_dir/ema_mae/adamw/ed0.999_lr1e-3_wd0.05_mask0.75 \
